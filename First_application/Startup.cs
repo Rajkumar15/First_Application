29ab2f6d8ada6fbdd9eeec61a804fde4cc26c5af
rajkumar <rajdorkhande5@gmail.com>
2017-11-15 14:50:29 +0530

    firstcommit

---
+++ First_application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(First_application.Startup))]
+namespace First_application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
